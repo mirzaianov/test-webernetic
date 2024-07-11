10f0351a4e1d6d4d6831293e463332b264e923f1
Riaz Mirzaianov <7o.mseJak@gmail.com>
2024-07-11 19:35:41 +0300

    feat: Complete Modal

---
+++ script.js
@@ -0,0 +1,23 @@
+const dialog = document.querySelector('.dialog');
+
+const openModal = () => {
+  dialog.showModal();
+};
+
+openModal();
+
+const closeModal = document.querySelector('.modal__close');
+
+closeModal.addEventListener('click', () => {
+  dialog.close();
+});
+
+const buttons = document.querySelectorAll('.modal__buttons');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    dialog.close();
+  });
+});
